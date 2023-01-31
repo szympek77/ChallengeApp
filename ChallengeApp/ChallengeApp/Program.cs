32dc50c27feb5d258e44cc1a593779afac4a120c
Sajmon <Sajmon@LAPTOP-3K7VME3B>
2023-01-31 21:21:47 +0100

    Zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,31 @@
-﻿var name = "Szymon";
-bool women = false;
-var age = 15;
+﻿
+var example = 55789600;
+string numberAsString = example.ToString();
+char[] chars = numberAsString.ToArray();
 
+List<char> numbers = new List<char>();
 
-if (women == true)
+numbers.Add('0');
+numbers.Add('1');
+numbers.Add('2');
+numbers.Add('3');
+numbers.Add('4');
+numbers.Add('5');
+numbers.Add('6');
+numbers.Add('7');
+numbers.Add('8');
+numbers.Add('9');
+
+Console.WriteLine("Podana liczba: " + example);
+
+foreach (var number in numbers)
 {
-    if (age < 30)
+    var count = 0;
+
+    foreach (var number2 in chars)
     {
-        Console.WriteLine("Kobieta, poniżej 30");
+        if (number == number2) count++;
     }
-    else if (age == 33 && name == "Ewa")
-    {
-        Console.WriteLine("Ewa, lat 33");
-    }
+    Console.WriteLine(number + " --> " + count);
 }
 
-else if (!women && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
